d2c3126eb678cdba0f7bcc679dfde802d5b29fc3
sebasalf <leon.sebastian@correo.unimet.edu.ve>
2025-03-17 18:20:17 -0400

    Update GalleryPage.jsx
    Acomodando botones

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -14,6 +14,9 @@ const GalleryPage = () => {
     { src: 'path/to/photo6.jpg', alt: 'Photo 6' },
     { src: 'path/to/photo7.jpg', alt: 'Photo 7' },
     { src: 'path/to/photo8.jpg', alt: 'Photo 8' },
+    { src: 'path/to/photo9.jpg', alt: 'Photo 9' },
+    { src: 'path/to/photo10.jpg', alt: 'Photo 10' },
+    { src: 'path/to/photo11.jpg', alt: 'Photo 11' },
     
     // Agrega más fotos según sea necesario
   ];
@@ -25,22 +28,24 @@ const GalleryPage = () => {
         <header className="gallery-header text-center mb-6">
           <h1 className="text-3xl text-[#889e19] font-bold">GALERÍA</h1>
         </header>
-        <div className="flex items-center justify-center bg-[#d9e4b1] rounded-lg shadow-md cursor-pointer hover:bg-[#c8d49a] transition-colors py-[1rem] w-[24rem] my-[1rem]">
-          <span className="text-[#889e19] text-xl font-bold">+ &nbsp; Agrega tu foto!</span>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {photos.map((photo, index) => (
-            <div key={index} className="relative group">
-              <img
-                src={photo.src}
-                alt={photo.alt}
-                className="w-full h-full object-cover rounded-lg shadow-md transition-transform transform group-hover:scale-105"
-              />
-              <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                <span className="text-white text-lg">{photo.alt}</span>
+        <div className="max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center">
+          <div className="flex items-center justify-center bg-[#d9e4b1] rounded-lg shadow-md cursor-pointer hover:bg-[#c8d49a] transition-colors py-[1rem] w-[20rem] my-[1rem]">
+            <span className="text-[#889e19] text-xl font-bold">+ &nbsp; Agrega tu foto!</span>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {photos.map((photo, index) => (
+              <div key={index} className="relative group">
+                <img
+                  src={photo.src}
+                  alt={photo.alt}
+                  className="w-full h-full object-cover rounded-lg shadow-md transition-transform transform group-hover:scale-105"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
+                  <span className="text-white text-lg">{photo.alt}</span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </main>
       <Footer />
